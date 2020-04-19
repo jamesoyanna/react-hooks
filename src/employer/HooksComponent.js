a6f37056791c10bfc32a2f9f119def156f147ae0
eshoiza <engrjayt200@gmail.com>
2020-04-19 23:14:02 +0100

    updated employee component

--- src/employer/HooksComponent.js
+++ src/employer/HooksComponent.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 export default function EmployeeDetails() {
-  const [name] = useState("Mayank");
+  const [name, setName] = useState("Mayank");
   const [age, setAge] = useState(31);
   const [designation] = useState("Senior Developer");
 
   function updateEmployeeAge() {
     setAge(age + 1);
+  }
+
+  function updateName(){
+      setName("James Oyanna")
   }
 
   return (
@@ -19,6 +23,8 @@ export default function EmployeeDetails() {
         onClick={updateEmployeeAge}
         value="Click To Update Age"
       />
+
+      <button type="button" onClick={updateName}>Change Name</button>
     </div>
   );
 }
